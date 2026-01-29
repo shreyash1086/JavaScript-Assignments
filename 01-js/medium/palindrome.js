8ee120bb1b1f777cb73fe499920f3acb7c36d6f7
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2026-01-29 10:02:10 +0530

    solved palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,6 +7,19 @@
 */
 
 function isPalindrome(str) {
+  
+  const newStr = str
+    .toLowerCase()
+    .split(/\W+/)
+    .join("");
+  
+  let i = 0;
+  let j = newStr.length - 1;
+  while (i <= j) {
+    if (newStr[i] !== newStr[j]) return false;
+    i++;
+    j--;
+  }
     return true;
 }
 
